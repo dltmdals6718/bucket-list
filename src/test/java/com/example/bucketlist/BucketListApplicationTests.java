95213490ff75844f4f17c4e146eca66105981d38
LSM <dltmdals6718@naver.com>
2024-06-16 16:31:43 +0900

    init

---
+++ src/test/java/com/example/bucketlist/BucketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bucketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
