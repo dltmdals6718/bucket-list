d8f9ee30073e0b47a73f655ee289934895c02c25
LSM <dltmdals6718@naver.com>
2024-06-26 13:26:34 +0900

    refactor: 인증 코드 정규 표현식 검사

--- src/main/java/com/example/bucketlist/dto/request/MemberSignupRequest.java
+++ src/main/java/com/example/bucketlist/dto/request/MemberSignupRequest.java
@@ -17,17 +17,15 @@ public class MemberSignupRequest {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String loginPwd;
 
-    @NotBlank(message = "비밀번호 재확인을 입력해주세요.")
     private String confirmPwd;
 
     @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
 
     @Pattern(regexp = "^[^\s@]+@[^\s@]+\\.[^\s@]+$", message = "올바른 이메일 형식을 입력해주세요.")
-    @NotBlank(message = "이메일 주소를 입력해주세요.")
     private String email;
 
-    @NotBlank(message = "인증 번호를 입력해주세요.")
+    @Pattern(regexp = "^[0-9]+$", message = "올바른 인증 번호를 입력해주세요.")
     private String emailCode;
 
 }
