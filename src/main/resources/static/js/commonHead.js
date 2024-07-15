350fd23e0b30640a20e62c19d157d4a237d1ba50
LSM <dltmdals6718@naver.com>
2024-07-15 18:51:10 +0900

    design: 네비게이션 바 반응형
    - 모바일 환경에서 메뉴 축약 버튼 생성
    - 뷰 포트 크기에 따라 프로필 이미지 위치 변경
    - 화면 너비에 따라 요소 조건부 표시

---
+++ src/main/resources/static/js/commonHead.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    function moveProfileImg() {
+
+        const profileImgHeader = document.getElementById('profileImgHeader');
+        const mobileMenuButton = document.getElementById('mobileMenuButton');
+        const expand = document.getElementById('expand');
+
+        if (window.innerWidth < 992) {
+            if (mobileMenuButton.previousSibling !== profileImgHeader) {
+                mobileMenuButton.parentNode.insertBefore(profileImgHeader, mobileMenuButton);
+            }
+        } else {
+            if (expand.nextSibling !== profileImgHeader) {
+                expand.parentNode.insertBefore(profileImgHeader, expand.nextSibling);
+            }
+        }
+    }
+
+    window.addEventListener('resize', moveProfileImg);
+    moveProfileImg();
+});
