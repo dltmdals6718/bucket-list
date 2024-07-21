85c0dfe86a27ca8a5be4b6f4cb69d196e2bf445f
LSM <dltmdals6718@naver.com>
2024-07-21 21:47:56 +0900

    design: 에러 메시지 디자인

--- src/main/resources/static/js/profileUpdate.js
+++ src/main/resources/static/js/profileUpdate.js
@@ -43,7 +43,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     const target = document.getElementById(key);
                     const errorMsg = document.createElement('div');
                     errorMsg.className = "error-message";
-                    errorMsg.textContent = responseJson[key];
+                    errorMsg.style.color = "orange";
+                    errorMsg.innerHTML =
+                        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='orange' style='margin: 5px' class='bi bi-exclamation-circle' viewBox='0 0 16 16'>\n" +
+                        "<path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16'/>\n" +
+                        "<path d='M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z'/>\n" +
+                        "</svg>" + responseJson[key];
                     target.insertAdjacentElement('afterend', errorMsg);
                 }
             }
@@ -133,7 +138,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     const target = document.getElementById(key);
                     const errorMsg = document.createElement('div');
                     errorMsg.className = "error-message";
-                    errorMsg.textContent = responseJson[key];
+                    errorMsg.style.color = "orange";
+                    errorMsg.innerHTML =
+                        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='orange' style='margin: 5px' class='bi bi-exclamation-circle' viewBox='0 0 16 16'>\n" +
+                        "<path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16'/>\n" +
+                        "<path d='M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z'/>\n" +
+                        "</svg>" + responseJson[key];
                     target.insertAdjacentElement('afterend', errorMsg);
                 }
 
