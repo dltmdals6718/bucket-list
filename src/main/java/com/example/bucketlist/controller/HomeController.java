a5936c7323854415d7a2edd57cff5bda18627cf9
LSM <dltmdals6718@naver.com>
2024-08-04 00:15:02 +0900

    feat: 게시글 목록 임시 출력

--- src/main/java/com/example/bucketlist/controller/HomeController.java
+++ src/main/java/com/example/bucketlist/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.example.bucketlist.controller;
 
+import com.example.bucketlist.domain.Poster;
+import com.example.bucketlist.repository.PosterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private PosterRepository posterRepository;
+
+    @Autowired
+    public HomeController(PosterRepository posterRepository) {
+        this.posterRepository = posterRepository;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Poster> all = posterRepository.findAll();
+        model.addAttribute("posters", all);
         return "home";
     }
 
