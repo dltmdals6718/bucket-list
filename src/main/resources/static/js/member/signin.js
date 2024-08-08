e4ead2fda882fe6679f6a435f3fade1a29bae7a2
LSM <dltmdals6718@naver.com>
2024-08-08 17:59:11 +0900

    design: 로그인 페이지

---
+++ src/main/resources/static/js/member/signin.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $("#kakaoLoginBtn").click(function () {
+        window.location = "/oauth2/authorization/kakao";
+    });
+
+    $("#googleLoginBtn").click(function () {
+        alert("준비중");
+    })
+});
