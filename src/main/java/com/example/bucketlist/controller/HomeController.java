bfdcd91a71b6d8e523cd0bad553271b508ea38d2
LSM <dltmdals6718@naver.com>
2024-09-22 15:33:51 +0900

    refactor: 메인 페이지에 게시글 노출

--- src/main/java/com/example/bucketlist/controller/HomeController.java
+++ src/main/java/com/example/bucketlist/controller/HomeController.java
@@ -1,13 +1,31 @@
 package com.example.bucketlist.controller;
 
+import com.example.bucketlist.dto.response.PosterOverviewResponse;
+import com.example.bucketlist.service.PosterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+
+    private final PosterService posterService;
+
+    @Autowired
+    public HomeController(PosterService posterService) {
+        this.posterService = posterService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+
+        PagedModel<PosterOverviewResponse> posterOverview = posterService.getPosterOverview(1, 5, null, null, null, null);
+        model.addAttribute("allPosters", posterOverview.getContent());
+
         return "home";
     }
 
